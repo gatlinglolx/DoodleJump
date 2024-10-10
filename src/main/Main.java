0d4fd92bf67bae879b9270b2a75591cd0aa840e3
gatlinglolx <shahov199@gmail.com>
2024-10-10 19:12:21 +0300

    changedExampleWithIfElse changedExampleWithWhile changedExampleWithFor MadeARenoteController

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,55 +4,73 @@ public class Main {
     public static void main(String[] args) {
 
         //FirstTask
-        String firstName = "Vladislav";
-        String lastName = "Shakhov";
-        String profession = "QA engineer";
-
-        System.out.println("My name is " + firstName);
-        System.out.println("My last name is " + lastName);
-        System.out.println("My profession is " + profession);
-
-        //SecondTask
-        byte byteVar = 100;
-        short shortVar = 10000;
-        int intVar = 100000;
-        long longVar = 100000L;
-
-        float floatVar = 10.5f;
-        double doubleVar = 20.99;
-
-        char charVar = 'A';
-
-        boolean booleanVar = true;
-
-        System.out.println("byte: " + byteVar);
-        System.out.println("short: " + shortVar);
-        System.out.println("int: " + intVar);
-        System.out.println("long: " + longVar);
-        System.out.println("float: " + floatVar);
-        System.out.println("double: " + doubleVar);
-        System.out.println("char: " + charVar);
-        System.out.println("boolean: " + booleanVar);
-
-        //ThirdTask
+        int score;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter some text: ");
-        String input = scanner.nextLine();
+        while (true) {
+            System.out.println("Please enter your score: ");
+            score = scanner.nextInt();
+            if (score >= 0 && score <= 100) {
+                break;
+            } else {
+                System.out.println("Некорректное значение, попробуйте снова.");
+            }
+        }
 
-        System.out.println("Text: " + input);
+        if (score >= 70) {
+            System.out.println("Congrats you've passed!");
+        } else {
+            System.out.println("Sorry you've failed!");
+        }
 
-        System.out.println("Длина строки: " + input.length());
-        System.out.println("Строка в верхнем регистре: " + input.toUpperCase());
-        System.out.println("Строка в нижнем регистре: " + input.toLowerCase());
-        System.out.println("Первый символ: " + input.charAt(0));
-        System.out.println("Последний символ: " + input.charAt(input.length() - 1));
-        System.out.println("Подстрока (с 0 по 4): " + input.substring(0, Math.min(input.length(), 5)));
-        System.out.println("Индекс символа 'a': " + input.indexOf('a'));
-        System.out.println("Содержит 'текст': " + input.contains("текст"));
-        System.out.println("Заменить 'текст' на 'замененный': " + input.replace("текст", "замененный"));
-        System.out.println("Строка без пробелов: " + input.replace(" ", ""));
+        //SecondTask
+        String userInput = "";
+        while (true) {
+            if (userInput.isEmpty()) {
+                System.out.println("Please enter smth: ");
+                userInput = scanner.nextLine();
+            }
 
-        scanner.close();
+            if (userInput.equals("exit")) {
+                System.out.println("Exiting");
+                break;
+            }
+
+            if (!userInput.isEmpty()) {
+                System.out.println("You've entered: " + userInput);
+            }
+        }
+
+        //ThirdTask
+        System.out.println("Please enter a number: ");
+        int x = scanner.nextInt();
+        int sum = 0;
+
+        if (x < 0) {
+            System.out.println("Negative numbers are not allowed");
+        }
+
+        else {
+            for (int i = 0; i <= x; i++) {
+                sum += i;
+            }
+            System.out.printf("The sum of the bumbers from 0 to %d is %d", x, sum);
+        }
+
+        //FourthTask
+        String[] channels = {"Russia 1", "Belarus 1", "NTV"};
+        while (true) {
+            System.out.println("Please enter the channel number (1 to 3 or 0 to exit): ");
+            int channel = scanner.nextInt();
+
+            if (channel == 0) {
+                System.out.println("TV is shutting down");
+                break;
+            }
+
+            if (channel >= 1 && channel < channels.length) {
+                System.out.println(channels[channel - 1]);
+            }
+        }
     }
 }
